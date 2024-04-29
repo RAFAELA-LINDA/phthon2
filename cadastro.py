4f03be4473e0f2c76db61f23b34591a679f25c03
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-04-29 12:14:32 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,1 +1,24 @@
+nome = input ("Digite seu nome" )
+idade = input ("Digite sua idade" )
+sexo = input ("Digite seu sexo" )
+
+nome2 = input ("Digite seu nome" )
+idade2 = input ("Digite sua idade" )
+sexo2 = input ("DIgite seu sexo" )
+
+nome3 = input ("Digite seu nome" )
+idade3 = input ("Digite sua idade" )
+sexo3 = input ("Digite seu sexo" )
+
+print ("nome:", nome )
+print ("idade:", idade )
+print ("sexo:", sexo )
+
+print ("nome:", nome2 )
+print ("idade:", idade2 )
+print ("sexo:", sexo2 )
+
+print ("nome:", nome3 )
+print ("idade:", idade3 )
+print ("sexo:", sexo3 )
 
