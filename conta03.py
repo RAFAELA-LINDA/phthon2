4293b2ca93f953a0a6b6661a58c53495bff9c78a
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-07-29 09:00:23 -0300

    Create conta03.py

---
+++ conta03.py
@@ -0,0 +1,15 @@
+class conta:
+    def __ini__(self, numero, titular, saldoinicial, limite):
+        self.numero = numero
+        self.titula = titular
+        self.saldo = saldoinicial
+        self.limite = limite
+
+
+        
+
+
+
+
+conta01 = conta(174, "Rafa", 600.00, 800.00)
+print(conta01)
